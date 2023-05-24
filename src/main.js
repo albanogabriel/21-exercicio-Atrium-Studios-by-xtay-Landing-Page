2a5f3305a7996eeefc83d3bb805014104de39758
Gabriel Albano <albanogabriel33@gmail.com>
2023-05-24 14:23:22 -0300

    create sections: faq, footer, header, and JS interactions

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-question]')
+
+    const heroSection = document.querySelector('.hero')
+    const alturaHero = heroSection.clientHeight
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY
+        // 754
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader()
+        } else {
+            exibeElementosDoHeader()
+        }
+    })
 
     for (let i = 0; i < buttons.length ; i++) {
         buttons[i].addEventListener('click', function(botao){
@@ -9,10 +24,11 @@ document.addEventListener('DOMContentLoaded', function() {
             aba.classList.add('shows__list--is-active')
             removeBotaoAtivo()
             botao.target.classList.add('shows__tabs__button--is-active')
-
-
-
         })
+    }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta)
     }
 })
 
@@ -30,4 +46,21 @@ function removeBotaoAtivo() {
     for(let i = 0; i < buttons.length; i++){
         buttons[i].classList.remove('shows__tabs__button--is-active')
     }
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open'
+    const elementoPai = elemento.target.parentNode
+
+    elementoPai.classList.toggle(classe)
+}
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden')
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden')
 }
